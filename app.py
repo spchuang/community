ad8676b08c258511a226f36e6062c5edeb27426a
Sam Chuang <sam4609ping1892@gmail.com>
2013-11-28 19:15:06 -0800

    remove sqlite from app

--- app.py
+++ app.py
@@ -12,11 +12,8 @@ from flask.ext.login import LoginManager, login_required,current_user
 #----------------------------------------
 # initialization
 #----------------------------------------
-DATABASE = 'community.db'
 DEBUG = True
 SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -28,78 +25,21 @@ login_manager.login_view = 'login'
 @app.before_request
 def before_request():
     g.user = current_user
-
-#Todo: model abstraction for database access s
-def connect_db():	
-   return sqlite3.connect(app.config['DATABASE'])
 	
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 	
-
+@login_manager.user_loader
+def load_user(id):
+   return Users().get_user(int(id))
+   
 #----------------------------------------
 # controllers
 #----------------------------------------
 @app.route('/')
 def show_entries():
-   cur = g.db.execute('select title, text from entries order by id desc')
-   entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-   return render_template('show_entries.html', entries=entries)
-    
-@app.route('/add', methods=['POST'])
-def add_entry():
-   if not session.get('logged_in'):
-      abort(401)
-   g.db.execute('insert into entries (title, text) values (?, ?)',
-               [request.form['title'], request.form['text']])
-   g.db.commit()
-   flash('New entry was successfully posted')
-   return redirect(url_for('show_entries'))
-
-'''
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-   error = None
-   if request.method == 'POST':
-      if request.form['username'] != app.config['USERNAME']:
-         error = 'Invalid username'
-      elif request.form['password'] != app.config['PASSWORD']:
-         error = 'Invalid password'
-      else:
-         session['logged_in'] = True
-         flash('You were logged in')
-      return redirect(url_for('login'))
-   	
-   if session.get('logged_in'):
-      return redirect(url_for('community'))
-   return render_template('login.html', error=error)
-
-@app.route('/logout')
-def logout():
-   session.pop('logged_in', None)
-   flash('You were logged out')
-   return redirect(url_for('login'))
-	
-	
-@app.route('/signup', methods=['GET','POST'])
-def signup():
-   if request.method == 'POST':
-      #Todo: form validation
-      g.db.execute('insert into user (first_name, last_name, user_name, password, gender, email) values (?, ?, ?, ?, ?, ?)',
-               [request.form['first_name'], request.form['last_name'], request.form['user_name'], request.form['password'], request.form['gender'], request.form['email']])
-      g.db.commit()
-      cur = g.db.execute('select user_name from user order by id desc')
-      #Todo: Error handling
-      print cur.fetchall()
-      
-   #return redirect(url_for('/home'))
-   return render_template('signup.html')
-'''
-
-@login_manager.user_loader
-def load_user(id):
-   return Users().get_user(int(id))
+   return redirect(url_for('home'))
 
 @app.route('/home')
 def home():
@@ -114,6 +54,8 @@ app.add_url_rule('/logout',   methods=['GET'],           view_func=account.logou
 app.add_url_rule('/signup',   methods=['GET', 'POST'],   view_func=account.signup)
 app.add_url_rule('/profile',  methods=['GET'],           view_func=account.profile)
 app.add_url_rule('/community',methods=['GET'],           view_func=community.community)
+
+
 
 
 #----------------------------------------
