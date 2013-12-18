03d153857157b854910fd32cff8ece7307198640
Justin Wei <justinwei@Justins-MacBook-Air.local>
2013-12-18 14:59:06 -0800

    Made some structural changes to event code

--- src/controllers/api/calendar.py
+++ src/controllers/api/calendar.py
@@ -9,26 +9,25 @@ api = Blueprint('calendar', __name__, url_prefix='/api/calendar')
 @api.route('/list', methods=['GET'])
 @login_required
 def list():
-   type = request.args.get('get') or 'community'
-
-   if type == 'community':
-      c_id = request.args.get('c_id')
-      events = Event().query.filter_by(community_id= c_id).all()
-      return jsonify(success = True, data= [
-               {'id': e.id, 'name': e.name, 'date': e.date, 'description': e.description}
-               for e in events
-          ])
-   elif type == g.user.user_name:
+   c_id = request.args.get('c_id')
+   if(c_id == 'all'):
       events = get_event_list(g.user)
       return jsonify(success = True, data= [
-               {'id': e.Event.id, 'name': e.Event.name, 'date': e.Event.date, 'description': e.Event.description}
+               e.Event.serialize
+               for e in events
+          ])
+   else:
+      events = Event().query.filter_by(community_id= c_id).all()
+      return jsonify(success = True, data= [
+               e.serialize
                for e in events
           ])
 
 
-@api.route('/create',methods=['POST'])
+
+@api.route('/new_event',methods=['POST'])
 @login_required
-def create_event():
+def new_event():
    c_id = request.args.get('c_id')
    if c_id is None:
       return jsonify(success = False, errors = "What community?")
@@ -36,11 +35,27 @@ def create_event():
    form = CreateEventForm()
    if form.validate_on_submit():
       new_event = Event(name   = form.name.data, 
-      							date = form.date.data,
-                                description  = form.description.data)
+      							    start_on = form.start.data,
+                        end_on = form.end.data,
+                        created_by = g.user.id,
+                        description  = form.description.data)
       c = Community().query.filter_by(id=c_id).first()
       c.create_event(new_event)
       db.session.add(c)
       db.session.commit()
       return jsonify(success = True)
    return jsonify(success = False, errors = form.errors)
+
+"""
+@api.route('/update_event',methods=['POST'])
+@login_required
+def update_event():
+"""
+
+
+@api.route('/delete_event',methods=['POST'])
+@login_required
+def delete_event():
+   print "yes"
+   c_id = request.args.get('c_id')
+
